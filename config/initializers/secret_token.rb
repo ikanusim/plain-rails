7acb6da78926644e036fd37f2a1f377b16ca1c17
Daniel Hüsch <daniel.huesch@gmail.com>
2011-02-14 09:51:20 +0100

    wip

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlainRails::Application.config.secret_token = '7f7002b11e030a2ae9fde9f53a27c686e808ca80e60090f5646d09057059615be09ecb6bdf7e650582766c5b1be89630a41ea93f7638338e94854bec591fcf45'
